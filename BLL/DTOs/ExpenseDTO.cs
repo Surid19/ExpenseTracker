8b7aa737063b444f062f99c6dda70a112a97b675
Md Toyabur Rahaman <70028265+Surid19@users.noreply.github.com>
2024-10-30 17:09:31 +0600

    Add files via upload

---
+++ BLL/DTOs/ExpenseDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BLL.DTOs
+{
+    public class ExpenseDTO
+    {
+        public int Id { get; set; }
+        [Required]
+        public decimal Amount { get; set; }
+        [Required]
+        public string Category { get; set; }
+        [Required]
+        public DateTime Date { get; set; } 
+        [Required]
+        public string Description { get; set; } 
+        public bool IsRecurring { get; set; } 
+    }
+}
---
+++ BLL/DTOs/UserDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BLL.DTOs
+{
+    public class UserDTO
+    {
+        public string Uname { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+    }
+}
